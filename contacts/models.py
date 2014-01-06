fa1624be536bacbe1670a541b86afd21a64addc4
Oerb <bjoern@oerb.de>
2014-01-06 20:35:10 +0100

    some Meta an typo in Contact Model

--- contacts/models.py
+++ contacts/models.py
@@ -31,8 +31,8 @@ class Category(models.Model):
     ca_description = models.TextField(verbose_name=u'Description', blank=True)
 
     class Meta:
-        verbose_name = u'category'
-        verbose_name_plural = u'categories'
+        verbose_name = u'Category'
+        verbose_name_plural = u'Categories'
 
     def __unicode__(self):
         return self.ca_name
@@ -42,9 +42,13 @@ class CondtactData(models.Model):
     Contactdata Model
     for phone, fax, email etc.
     """
-    cd_contacttype_id = models.ForeignKey(Contacttype)
+    cd_contacttype_id = models.ForeignKey(ContactType)
     cd_textfield = models.CharField(255)
     cd_address_id = models.ForeignKey(Address)
+
+    class Meta:
+        verbose_name= u'Contact Data'
+        verbose_name_plural= u'Contact Data'
 
     def __unicode__(self):
         return self.cd_contacttype_id.ct_name
@@ -55,7 +59,7 @@ class ContactDataFulltext(models.Model):
     Contactdata Fulltext Model
     for Fulltext elements
     """
-    cf_contacttype_id = models.ForeignKey(Contacttype)
+    cf_contacttype_id = models.ForeignKey(ContactType)
     cf_textfield = models.TextField()
     cf_address_id = models.ForeignKey(Address, editable=False)
 
@@ -68,9 +72,10 @@ class ContactType(models.Model):
     Contacttype Model
     Types like phone, fax, email, etc.
     """
-    ct_name = models.CharField(u'name', max_length=20)
-    ct_notice = models.CharField(u'notice', max_length=255)
+    ct_name = models.CharField(verbose_name=u'Name', max_length=20)
+    ct_info = models.CharField(verbose_name=u'Notice', max_length=255)
     ct_category_id = models.ForeignKey(Category)
+    ct_sort_id = models.IntegerField(verbose_name=u'sort_id')
 
     def __unicode__(self):
         return self.ct_name
