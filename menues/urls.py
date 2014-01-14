a6018726804ec8a2554d6d24058b8233963be7bb
Oerb <bjoern@oerb.de>
2014-01-14 20:49:12 +0100

    connecting central urls.py with the apps urls.py

---
+++ menues/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Login Forms
+    url(r'^login/$', 'menues.views.login_page',
+        name="login"),
+    url(r'^logout/$', 'menues.views.logout_view',
+        name="logout"),
+    # Meta Infos etc.
+    url(r'^impressum/$', 'menues.views.impressum', name='impressum'),
+    url(r'^contacts/', include('contacts.urls')),
+    url(r'^menues/', include('menues.urls')),
+)
