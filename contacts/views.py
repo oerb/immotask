9353a865436fcc94f4891e5163958c548fa6bcb5
Oerb <bjoern@oerb.de>
2014-03-04 17:44:22 +0100

    bootstrap tab testing 1

--- contacts/views.py
+++ contacts/views.py
@@ -6,5 +6,6 @@ def index(request, item_id):
         address = get_object_or_404(Address, pk=item_id)
         adr_fulltextfields = ContactDataFulltext.objects.filter(cf_address_id=address.id)
         adr_data = ContactData.objects.filter(cd_address_id=address.id)
+        catagories = Category.objects.all()
         return render(request, 'contacts/detail.html',{'address':address, 'adr_fulltextfields':adr_fulltextfields,
-                                                  'adr_data':adr_data})
+                                                  'adr_data':adr_data, 'catagories':catagories})
