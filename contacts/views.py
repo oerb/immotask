05d9ce3b6f033d36f7084c974b3572b7f624725f
Oerb <bjoern@oerb.de>
2014-03-04 18:30:14 +0100

    bootstrap tab testing 3 - ToTo

--- contacts/views.py
+++ contacts/views.py
@@ -9,3 +9,11 @@ def index(request, item_id):
         catagories = Category.objects.all()
         return render(request, 'contacts/detail.html',{'address':address, 'adr_fulltextfields':adr_fulltextfields,
                                                   'adr_data':adr_data, 'catagories':catagories})
+# TODO: Url, TAB and Item Id using/filtering in template and view
+def ct_detail_tab(request, item_id):
+        address = get_object_or_404(Address, pk=item_id)
+        adr_fulltextfields = ContactDataFulltext.objects.filter(cf_address_id=address.id)
+        adr_data = ContactData.objects.filter(cd_address_id=address.id)
+        catagories = Category.objects.all()
+        return render(request, 'contacts/detailtab.html',{'address':address, 'adr_fulltextfields':adr_fulltextfields,
+                                                  'adr_data':adr_data, 'catagories':catagories})
