e4f4411b21c1510897128cf2aff7e209445ee1c2
Oerb <bjoern@oerb.de>
2014-03-05 18:22:36 +0100

    right_menu example, contact.detail deleted now in detailtab

--- contacts/urls.py
+++ contacts/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # URL for contact in Detail
-    url(r'^(?P<address_id>\d+)/$', 'contacts.views.ct_detail', name='contact_detail'),
+
     url(r'^(?P<address_id>\d+)/(?P<category_id>\d+)/$', 'contacts.views.ct_detail_tab', name='contact_detailtab'),
     url(r'^view/$', 'contacts.views.proj_contacts', name='proj_contacts'),
     url(r'^new_contact/$', 'contacts.views.new_contact', name='new_contact'),
--- contacts/views.py
+++ contacts/views.py
@@ -3,13 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from contacts.forms import ContactForm
 
-def ct_detail(request, address_id):
-        address = get_object_or_404(Address, pk=address_id)
-        adr_fulltextfields = ContactDataFulltext.objects.filter(cf_address_id=address.id)
-        adr_data = ContactData.objects.filter(cd_address_id=address.id).order_by('cd_contacttype_id__ct_sort_id')
-        categories = Category.objects.all()
-        return render(request, 'contacts/detail.html',{'address': address, 'adr_fulltextfields': adr_fulltextfields,
-                                                  'adr_data': adr_data, 'categories': categories})
+
 # TODO: Url, TAB and Item Id using/filtering in template and view
 def ct_detail_tab(request, address_id, category_id):
         category_id = int(category_id)
