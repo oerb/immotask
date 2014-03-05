8fe6d3a238ee3ce72e7a3b9fadb3a7ab4fee3f17
Oerb <bjoern@oerb.de>
2014-03-05 15:01:59 +0100

    bootstrap view - works simple

--- contacts/urls.py
+++ contacts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     # URL for contact in Detail
     url(r'^(?P<address_id>\d+)/$', 'contacts.views.ct_detail', name='contact_detail'),
     url(r'^(?P<address_id>\d+)/(?P<catagory_id>\d+)/$', 'contacts.views.ct_detail_tab', name='contact_detailtab'),
+    url(r'^view/$', 'contacts.views.proj_contacts', name='proj_contacts'),
 )
--- contacts/views.py
+++ contacts/views.py
@@ -7,8 +7,8 @@ def ct_detail(request, address_id):
         adr_fulltextfields = ContactDataFulltext.objects.filter(cf_address_id=address.id)
         adr_data = ContactData.objects.filter(cd_address_id=address.id)
         catagories = Category.objects.all()
-        return render(request, 'contacts/detail.html',{'address':address, 'adr_fulltextfields':adr_fulltextfields,
-                                                  'adr_data':adr_data, 'catagories':catagories})
+        return render(request, 'contacts/detail.html',{'address': address, 'adr_fulltextfields': adr_fulltextfields,
+                                                  'adr_data': adr_data, 'catagories': catagories})
 # TODO: Url, TAB and Item Id using/filtering in template and view
 def ct_detail_tab(request, address_id, catagory_id):
         catagory_id = int(catagory_id)
@@ -16,5 +16,10 @@ def ct_detail_tab(request, address_id, catagory_id):
         adr_fulltextfields = ContactDataFulltext.objects.filter(cf_address_id=address.id)
         adr_data = ContactData.objects.filter(cd_address_id=address.id)
         catagories = Category.objects.all()
-        return render(request, 'contacts/detailtab.html',{'address':address, 'adr_fulltextfields':adr_fulltextfields,
-                                                  'adr_data':adr_data, 'catagory_id':catagory_id, 'catagories':catagories})
+        return render(request, 'contacts/detailtab.html', {'address': address, 'adr_fulltextfields': adr_fulltextfields,
+                                                  'adr_data': adr_data, 'catagory_id': catagory_id, 'catagories': catagories})
+# TODO: Needs to be filtered by project ID, Needs more Addresselements
+def proj_contacts(request):
+    adr_data = ContactData.objects.all()
+    addresses = Address.objects.all()
+    return render(request, 'contacts/proj_contacts.html', {'adr_data': adr_data, 'addresses': addresses})
