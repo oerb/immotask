a2105e4fdab4a39606a877f19a78b88ab2854e0f
Oerb <bjoern@oerb.de>
2014-03-07 18:59:24 +0100

    add new Contact form works

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,5 @@
 from contacts.models import Address, Category, ContactType, ContactData, ContactDataFulltext
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from contacts.forms import ContactForm, contact_form_dyn_expand
 
@@ -12,25 +12,32 @@ def ct_detail_tab(request, address_id, category_id):
         adr_data = ContactData.objects.filter(cd_address_id=address.id).order_by('cd_contacttype_id__ct_sort_id')
         categories = Category.objects.all()
         return render(request, 'contacts/detailtab.html', {'address': address, 'adr_fulltextfields': adr_fulltextfields,
-                                                  'adr_data': adr_data, 'category_id': category_id, 'categories': categories})
+                                                  'adr_data': adr_data, 'category_id': category_id,
+                                                  'categories': categories})
+
 # TODO: Needs to be filtered by project ID, Needs more Addresselements
 def proj_contacts(request):
     adr_data = ContactData.objects.all()
     addresses = Address.objects.all()
+
     return render(request, 'contacts/proj_contacts.html', {'adr_data': adr_data, 'addresses': addresses})
 
 def new_contact(request):
-    # TODO: Not tested, list of all ContactsTypes to fill
     message = None
-    contacttypes = ContactType.objects.all()
     if request.method == "POST":
         form = ContactForm(request.POST)
-        contact_form_dyn_expand(form)
         if form.is_valid():
-            searchname = request.POST['searchname']
-            email = request.POST['email']
+            adr = Address(adr_searchname=form.cleaned_data['searchname'],
+                          adr_email=form.cleaned_data['email'])
+            adr.save()
+            contacttypes = ContactType.objects.all()
+            for ct_type in contacttypes:
+                ctdata = ContactData(cd_contacttype_id= ct_type,
+                                     cd_textfield = form.cleaned_data['{index}'.format(index=ct_type.id)],
+                                     cd_address_id = adr)
+                ctdata.save()
+
+            return redirect('proj_contacts')
     else:
         form = ContactForm()
-        contact_form_dyn_expand(form)
-        #print dir(form.fields.append()) #"\n".join(form.__dict__)
-    return render(request, 'contacts/new_contact.html', {'message': message, 'form': form, 'contacttypes': contacttypes})
+    return render(request, 'contacts/new_contact.html', {'message': message, 'form': form})
