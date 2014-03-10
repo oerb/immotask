5ce80f65e1272287a6cd7fa2dfe3f4d96e5854ae
Oerb <bjoern@oerb.de>
2014-03-10 13:38:30 +0100

    Contact Edit simple works

--- contacts/urls.py
+++ contacts/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
 
     url(r'^(?P<address_id>\d+)/(?P<category_id>\d+)/$', 'contacts.views.ct_detail_tab', name='contact_detailtab'),
     url(r'^view/$', 'contacts.views.proj_contacts', name='proj_contacts'),
-    url(r'^new_contact/$', 'contacts.views.new_contact', name='new_contact'),
+    url(r'^new/$', 'contacts.views.new_contact', name='new_contact'),
+    url(r'^edit/(?P<address_id>\d+)$', 'contacts.views.edit_contact', name='edit_contact'),
 )
