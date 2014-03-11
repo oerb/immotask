3eb368bf3f419defe34ee27d720bbd8b4f1cf8a3
Oerb <bjoern@oerb.de>
2014-03-11 17:46:07 +0100

    some Modeling

--- tasks/models.py
+++ tasks/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 from docs.models import Doc
+from contacts.models import Address
 
 # Create your models here.
+
+
 class AuthoriseStruct(models.Model):
     """
     Authorise Structure
     who must sign the task so it is done and where it is shown how
     """
     as_name = models.CharField(verbose_name=u'Name', max_length=100)
-    as_info = models.CharField(verbose_name=u'Info', max_length=250)
+    as_info = models.CharField(verbose_name=u'Info', max_length=250, blank=True)
     # as_user1_id = models.ForeignKey(User) TODO: Userid add
-    as_user1_level = models.IntegerField(blank=True)
+    as_user1_level = models.IntegerField(blank=True, null=True)
     as_u1_name = models.CharField(verbose_name=u'L1 Name', max_length=100, blank=True)
     # as_user2_id = models.ForeignKey(User) TODO: Userid add
-    as_user2_level = models.IntegerField(blank=True)
+    as_user2_level = models.IntegerField(blank=True, null=True)
     as_u2_name = models.CharField(verbose_name=u'L2 Name', max_length=100, blank=True)
     # as_user3_id = models.ForeignKey(User) TODO: Userid add
-    as_user3_level = models.IntegerField(blank=True)
+    as_user3_level = models.IntegerField(blank=True, null=True)
     as_u3_name = models.CharField(verbose_name=u'L3 Name', max_length=100, blank=True)
     # as_user4_id = models.ForeignKey(User) TODO: Userid add
-    as_user4_level = models.IntegerField(blank=True)
+    as_user4_level = models.IntegerField(blank=True, null=True)
     as_u4_name = models.CharField(verbose_name=u'L4 Name', max_length=100, blank=True)
 
     def __unicode__(self):
@@ -32,7 +35,7 @@ class TaskType(models.Model):
     like advice, message, note, report
     """
     tt_name = models.CharField(verbose_name=u'Name', max_length=100)
-    tt_info = models.CharField(verbose_name=u'Name', max_length=250)
+    tt_info = models.CharField(verbose_name=u'Info', max_length=250, blank=True)
     tt_authstruct_id = models.ForeignKey(AuthoriseStruct)  # For a Structure in Signing the Task to get done
 
     def __unicode__(self):
@@ -47,6 +50,9 @@ class Task(models.Model):
     ta_longtxt = models.TextField(verbose_name=u'Meldungstext', blank= True)
     ta_date = models.DateTimeField() # for first edit TODO: auto set Date
     ta_priority = models.IntegerField(blank=True)
+    ta_adrid_ext = models.ForeignKey(Address)  # External
+    # ta_adrid_int = models.ForeignKey(User)  # Internal TODO: Userid ADD
+    ta_tasktype = models.ForeignKey(TaskType)
     # ta_parent = models.ForeignKey(self, blank=True) TODO: Self ForeignKey ...
 
     def __unicode__(self):
