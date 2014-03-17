f95e1297e98405f68886517867c90bb43237c35c
Oerb <bjoern@oerb.de>
2014-03-17 19:22:08 +0100

    NewTask Forms testing nice Datepicking

--- tasks/forms.py
+++ tasks/forms.py
@@ -10,19 +10,23 @@ import datetime
 class TaskForm(forms.Form):
     shorttxt = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Kurztext'}))
     longtxt = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Langtext'}))
-    begin = forms.DateField(initial=datetime.date.today, widget=SelectDateWidget(attrs={'class': 'form-control'}))
+    begin = forms.DateField(initial=datetime.date.today, widget=SelectDateWidget())
     begintime = forms.TimeField(required=False, widget=forms.TimeInput(attrs={'class': 'form-control'}))
-    end = forms.DateField(initial=datetime.date.today, widget=SelectDateWidget(attrs={'class': 'form-control'}))
+    end = forms.DateField(initial=datetime.date.today, widget=SelectDateWidget())
     endtime = forms.TimeField(required=False, widget=forms.TimeInput(attrs={'class': 'form-control'}))
-    warn = forms.DateField(initial=datetime.date.today, widget=SelectDateWidget(attrs={'class': 'form-control'}))
+    warn = forms.DateField(initial=datetime.date.today, widget=SelectDateWidget())
     priority_choices = [
         ('1', 'niedrig'),
         ('2', 'mittel'),
         ('3', 'hoch')
     ]
     priority = forms.ChoiceField(choices=priority_choices, required=True, initial='1')
+    priority.widget.attrs['class']='form-control'
     adr_from = forms.ModelChoiceField(queryset=Address.objects.all())
+    adr_from.widget.attrs['class']='form-control'
     adr_to = forms.ModelChoiceField(queryset=Address.objects.all())
+    adr_to.widget.attrs['class']='form-control'
     tasktype = forms.ModelChoiceField(queryset=TaskType.objects.all())
+    tasktype.widget.attrs['class']='form-control'
 
 
