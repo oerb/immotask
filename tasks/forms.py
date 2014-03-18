be7a770c21dd6bbfa95ebe3966c04daf5ec3b4b5
Oerb <bjoern@oerb.de>
2014-03-18 09:31:42 +0100

    NewTask Forms testing nice structure

--- tasks/forms.py
+++ tasks/forms.py
@@ -8,6 +8,12 @@ import datetime
 
 
 class TaskForm(forms.Form):
+    adr_from = forms.ModelChoiceField(queryset=Address.objects.all())
+    adr_from.widget.attrs['class']='form-control'
+    adr_to = forms.ModelChoiceField(queryset=Address.objects.all())
+    adr_to.widget.attrs['class']='form-control'
+    tasktype = forms.ModelChoiceField(queryset=TaskType.objects.all())
+    tasktype.widget.attrs['class']='form-control'
     shorttxt = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Kurztext'}))
     longtxt = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Langtext'}))
     begin = forms.DateField(initial=datetime.date.today, widget=SelectDateWidget())
@@ -22,11 +28,6 @@ class TaskForm(forms.Form):
     ]
     priority = forms.ChoiceField(choices=priority_choices, required=True, initial='1')
     priority.widget.attrs['class']='form-control'
-    adr_from = forms.ModelChoiceField(queryset=Address.objects.all())
-    adr_from.widget.attrs['class']='form-control'
-    adr_to = forms.ModelChoiceField(queryset=Address.objects.all())
-    adr_to.widget.attrs['class']='form-control'
-    tasktype = forms.ModelChoiceField(queryset=TaskType.objects.all())
-    tasktype.widget.attrs['class']='form-control'
+
 
 
