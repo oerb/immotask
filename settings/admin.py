9d56ad0999ec6fa65c6e19c87f3925bf0eb99764
Oerb <bjoern@oerb.de>
2014-03-18 17:32:26 +0100

    Models and admin.py for settings App

--- settings/admin.py
+++ settings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from settings.models import Setting
+
 
 # Register your models here.
+
+admin.site.register(Setting)
+
+
+
--- settings/models.py
+++ settings/models.py
@@ -9,7 +9,9 @@ class Setting(models.Model):
     """
     User Settings like current Project etc.
     """
-    se_current_proj = models.ForeignKey(Project, related_name=u'current Project ID', blank=True, null=True)
+    se_current_proj = models.ForeignKey(Project, verbose_name=u'current Project ID',
+                                        related_name=u'current Project ID',
+                                        blank=True, null=True)
     se_mailwarn = models.BooleanField(verbose_name=u'get Mail warnings')
     se_newtaskmail = models.BooleanField(verbose_name=u'get New Tasks as Mail')
-    se_address = models.ForeignKey(Address, blank=True, null=True)
+    se_address = models.ForeignKey(Address, verbose_name=u'Adresse', blank=True, null=True)
